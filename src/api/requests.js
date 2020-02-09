fe8ba559b9ace58d995552973918677604d432fb
Daniel Junior <oidanieljunior@gmail.com>
2020-02-09 18:19:11 -0300

    Return data on requests

--- src/api/requests.js
+++ src/api/requests.js
@@ -22,22 +22,22 @@ export default class RequestsFactory {
   }
 
   get(url, config = {}) {
-    return axios.get(`${this.baseUrl}${url}`, this.config(config));
+    return axios.get(`${this.baseUrl}${url}`, this.config(config)).then((result) => result.data);
   }
 
   post(url, data = {}, config = {}) {
-    return axios.post(`${this.baseUrl}${url}`, data, this.config(config));
+    return axios.post(`${this.baseUrl}${url}`, data, this.config(config)).then((result) => result.data);
   }
 
   patch(url, config = {}) {
-    return axios.patch(`${this.baseUrl}${url}`, this.config(config));
+    return axios.patch(`${this.baseUrl}${url}`, this.config(config)).then((result) => result.data);
   }
 
   delete(url, config = {}) {
-    return axios.delete(`${this.baseUrl}${url}`, this.config(config));
+    return axios.delete(`${this.baseUrl}${url}`, this.config(config)).then((result) => result.data);
   }
 
   put(url, data = {}, config = {}) {
-    return axios.put(`${this.baseUrl}${url}`, data, this.config(config));
+    return axios.put(`${this.baseUrl}${url}`, data, this.config(config)).then((result) => result.data);
   }
 }
